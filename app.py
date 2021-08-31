4d6e5b1c5345c50356f869c857ac0274667c45c4
NicholasPage <pagenw142@gmail.com>
2021-08-30 20:21:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,21 @@ data += object_data
 
 recipes = data
 
+@app.route('/recipe/api/v1.0/recipe/<int:id>', methods=['GET'])
+def Get_Recipe(id):
+    """Return recipe by ID"""
+    if id not in range(1,len(recipes)):
+        abort(404)
+    recipe = recipes[id-1]
+    return jsonify({'recipe': recipe})
 
+	
 @app.route('/recipe/api/v1.0/recipe/allrecipes', methods=['GET'])
 def Get_Allrecipes():
     """Return all recipes"""
     return jsonify(recipes)
 
-
+	
 @app.route('/recipe/api/v1.0/recipe/recipeoftheday', methods=['GET'])
 def Get_Recipeoftheday():
     """Return a random recipe"""
@@ -36,22 +44,31 @@ def Get_Recipeoftheday():
     recipe = random.choice(recipes)
     return jsonify({'Meal of the day': recipe['name']}, {'Ingredients': recipe['ingredients']}, {'Difficulty Level': recipe['difficulty']})
 
-
+	
 @app.route('/recipe/api/v1.0/newrecipe', methods=['POST'])
 def Create_Recipe():
     """Adds a recipe to the list"""
 	#Example
 	#curl -H "Content-Type: application/json" -X POST -d '{"name":"recipename","ingredients":"ingredient1, ingredient2, ingredient3","difficulty":"difficultylevel"}' http://localhost:5000/recipe/api/v1.0/newrecipe
-    if not request.json or not 'text' in request.json:
+    if not request.json or not "name" in request.json:
         abort(400)
     recipe = {
-        'name': request.json.get('name', ""),
-        'ingredients': request.json.get('ingredients', ""),
-	'difficulty': request.json.get('difficulty', "")
+        "name": request.json.get("name", ""),
+        "ingredients": request.json.get("ingredients", ""),
+	"difficulty": request.json.get("difficulty", "")
     }
     recipes.append(recipe)
     return jsonify({'recipe': recipe}), 201
+	
 
+@app.route('/recipe/api/v1.0/DeleteRecipeById/<int:id>', methods=['DELETE'])
+def Delete_RecipeById(id):
+    """Delete a recipe with the provided recipeID"""
+    if id not in range(1,len(recipes)):
+        abort(404)
+    recipe = recipes[id-1]
+    recipes.remove(recipe)
+    return jsonify({'Successfully removed the recipe': recipe['name']})
 
 
 if __name__ == '__main__':
